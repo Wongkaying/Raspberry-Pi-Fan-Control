53c5d938ed6c47a8c6c982efe2f9f16fe3c13d57
Wongkaying <wongkayingshenfu@gmail.com>
2023-03-20 16:49:10 +0100

    Improvement of the logging +  expand logs

--- Pi_Fan.py
+++ Pi_Fan.py
@@ -1,7 +1,6 @@
 import subprocess
 import time
 import os
-import logging
 
 from datetime import datetime
 from gpiozero import OutputDevice, CPUTemperature
@@ -10,11 +9,15 @@ GET_COOL = 45  # (degrees Celsius) Fan kicks on at this temperature.
 BE_COOL = 40  # (degress Celsius) Fan shuts off at this temperature.
 SLEEP_INTERVAL = 5  # (seconds) How often we check the core temperature.
 GPIO_PIN = 17  # Which GPIO pin you're using to control the fan.
-LOGS = "" # Path where the log file should be stored. Examlple: home/pi/logs/examplelog.log -- Leave empty to disgart ""
+LOGS = "" # Path where the log file should be stored. Examlple: home/pi/logs/example_log.log -- Leave empty to disgart ""
+TEMP_LOGS = "" # Path where the temperature log file should be stored. Examlple: home/pi/logs/example_temp_log.log -- Leave empty to disgart ""
 
 # Get current time
-def get_time():
-    return str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+def get_time(formate):
+    if formate == "shot":
+        return str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    else:
+        return str(datetime.now().strftime('%Y-%m-%d'))
 
 # Function to write the logs based on the log level
 def write_log(logtext, level=0):
@@ -22,23 +25,35 @@ def write_log(logtext, level=0):
         return
     else:
         try:
-            logging.basicConfig(filename=LOGS, level=logging.DEBUG, format='')
+            f = open(LOGS, "a")
 
             if level == 0:
-                text = '[Info] - ' + get_time() + ' - ' + logtext
-                logging.debug(text)
+                text = '[Info] - ' + get_time("") + ' - ' + logtext
+                f.write(text+'\n')
             elif level == 1:
-                text = '[SUCCESS] - ' + get_time() + ' - ' + logtext
-                logging.debug(text)
+                text = '[SUCCESS] - ' + get_time("") + ' - ' + logtext
+                f.write(text+'\n')
             elif level == 2:
-                text = '[WARNING] - ' + get_time() + ' - ' + logtext
-                logging.debug(text)
+                text = '[WARNING] - ' + get_time("") + ' - ' + logtext
+                f.write(text+'\n')
             elif level == 3:
-                text = '[ERROR] - ' + get_time() + ' - ' + logtext
-                logging.debug(text)
+                text = '[ERROR] - ' + get_time("") + ' - ' + logtext
+                f.write(text+'\n')
         except (FileNotFoundError):
-                print("File path not found")
+                print("LOGS path not found")
                 return
+    if TEMP_LOGS == "":
+        return
+    else:
+        try:
+            f = open(TEMP_LOGS, "a")
+
+            if level == 4:
+                text = '[TEMPERATURE OF THE DAY] - ' + get_time("short") + ' - ' + logtext
+                f.write(text+'\n')
+        except (FileNotFoundError):
+            print("TEMP_LOGS path not found")
+            return
 
 # Get temperature
 def get_temp():
@@ -54,6 +69,10 @@ def get_temp():
 print("Wongkaying's Pi_Fan started")
 write_log("Wongkaying's Pi_Fan started", 0)
 
+def temp_list():
+    temp_list = []
+    return temp_list
+
 # Fan control
 if __name__ == '__main__':
     # Secure
@@ -63,16 +82,27 @@ if __name__ == '__main__':
     while True:
         temp = get_temp()
         temp_str = str(temp)
+        if TEMP_LOGS == "":
+            continue
+        else:
+            if len(temp_list) < 5:
+                temp_list.append(temp)
+                print(len(temp_list))
+                write_log("Counter: " + str(len(temp_list)) + " with Value: " + temp_str, 4)
+            else:
+                avg = sum(temp_list) / len(temp_list)
+                print(str(avg).split(".")[0])
+                temp_list.clear()
         # Start the fan if the temperature has reached the limit and the fan
         # isn't already running.
         # NOTE: `fan.value` returns 1 for "on" and 0 for "off"
         if temp > GET_COOL and not fan.value:
-            write_log("Fan kicks on at " + temp_str, 1)
+            write_log("Fan kicks on at " + temp_str + "°C", 1)
             fan.on()
 
         # Stop the fan if the fan is running and the temperature has dropped
         # to 10 degrees below the limit.
         elif fan.value and temp < BE_COOL:
-            write_log("Fan kicks off at " + temp_str, 1)
+            write_log("Fan kicks off at " + temp_str + "°C", 1)
             fan.off()
         time.sleep(SLEEP_INTERVAL)
